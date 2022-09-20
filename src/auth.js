272c8bf911f8bd454ec48e65ceb85789b96329a0
Annie Huynh <z5391709@ad.unsw.edu.au>
2022-09-20 17:33:28 +1000

    Created authRegisterV1

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,14 @@
 // authLoginV1 stub function
 function authLoginV1(email, password) {
-    return {
-      authUserId: 1,
-    }
+  return {
+    authUserId: 1,
   }
+}
   
+
+// authRegisterV1 stub function
+function authRegisterV1(email, password, nameFirst, nameLast) {
+  return {
+    authUserId: 1,
+  }
+}
