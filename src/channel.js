9a4d2516cada1b27ca42bfaebca6ad737a1cc31a
Xingjian Dong <z5221888@ad.unsw.edu.au>
2022-09-21 11:42:52 +1000

    done 2 channel functions

---
+++ src/channel.js
@@ -0,0 +1,31 @@
+// channelDetailsV1 stub function
+function channelDetailsV1(authUserId, channelId) {
+  return {
+    name: 'Hayden',
+    ownerMembers: [
+      {
+        uId: 1,
+        email: 'example@gmail.com',
+        nameFirst: 'Hayden',
+        nameLast: 'Jacobs',
+        handleStr: 'haydenjacobs',
+      }
+    ],
+    allMembers: [
+      {
+        uId: 1,
+        email: 'example@gmail.com',
+        nameFirst: 'Hayden',
+        nameLast: 'Jacobs',
+        handleStr: 'haydenjacobs',
+      }
+    ],
+  }
+}
+
+// channelJoinV1 stub function
+function channelJoinV1(authUserId, channelId) {
+  return {
+
+  }
+}
