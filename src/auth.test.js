f697e2a879cabe51f3f20ce22614f94c1139c258
Muhammad Lodhi <z5260271@ad.unsw.edu.au>
2022-10-05 07:44:18 +1100

    addings tests for authRegisterV1

---
+++ src/auth.test.js
@@ -0,0 +1,49 @@
+import {authRegisterV1} from './auth';
+import {clearV1} from './other.js';
+
+describe('Test Suite for auth functions', () => {
+
+
+
+    afterEach(() =>  {  
+        clearV1();
+    });
+
+    test('Successful authRegisterV1', () => {   
+        expect(authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', 'moniker')).toStrictEqual(expect.any(Number));    
+    });
+
+    test('error passing invalid email through authRegisterV1', () => {   
+        expect(authRegisterV1('monikereffewfw', 'pvssword', 'real', 'moniker')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    test('error passing already registered email through authRegisterV1', () => {
+        authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', 'moniker')
+        expect(authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', 'moniker')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    test('error passing invalid password through authRegisterV1 (less than 6 characters)', () => {   
+        expect(authRegisterV1('moniker@hotmail.com', 'pvss', 'real', 'moniker')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    test('error passing invalid first name through authRegisterV1 (too short)', () => {   
+        expect(authRegisterV1('moniker@hotmail.com', 'pvssword', '', 'moniker')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    test('error passing invalid first name through authRegisterV1 (too long)', () => {   
+        expect(authRegisterV1('moniker@hotmail.com', 'pvssword', 'weqrewqreweqrewqreweqrewqreweqrewqreweqrewqreweqrewqre', 'moniker')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    test('error passing invalid last name through authRegisterV1 (too short)', () => {   
+        expect(authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', '')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    test('error passing invalid last name through authRegisterV1 (too long)', () => {   
+        expect(authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', 'weqrewqreweqrewqreweqrewqreweqrewqreweqrewqreweqrewqre')).toStrictEqual({error: expect.any(String)});    
+    });
+
+    // test('Testing if created authUserId is unique', () => {
+    //     const case_1 = authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', 'moniker');
+        
+    // });
+});
--- src/other.js
+++ src/other.js
@@ -1,9 +1,15 @@
+import {
+    setData,
+} from './dataStore.js'
+
 export function clearV1(){
-    data = {
+    
+    const data = {
         users: [   
         ],
         channels: [
         ]    
     };
+    setData(data);
     return;
 }
