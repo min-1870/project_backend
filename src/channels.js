7193838d03baa78f9b51f4334bb6ceccc9fc85d6
Minseok_Kim <200134kms@gmail.com>
2022-10-06 04:25:06 +0700

    added the function channelsListAllV1

--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,5 @@
+import { getData } from './dataStore'
+
 //channelsCreateV1 stub fucntion
 function channelsCreateV1( authUserId, name, isPublic ){
     return {
@@ -18,13 +20,23 @@ function channelsListV1( authUserId ){
   }
 
 //channelsListAllV1 stub fucntion
-function channelsListAllV1( authUserId ){
-    return {
-        channels: [
-          {
-            channelId: 1,
-            name: 'My Channel',
-          }
-        ],
+export function channelsListAllV1( authUserId ){
+    let data = getData()
+    
+    if (!isAuthUserIdValid(authUserId, data)){    //if the uesr ID is not valid return error
+      return { error: 'error' }; 
+    }
+
+    return {        //return every channels in the data without  Id & name only
+        channels: data.channels.map(({channelId, name}) => ({channelId, name}))
       }
   }
+
+
+function isAuthUserIdValid(authUserId, data) {
+    return getUser(authUserId, data) != null
+}
+
+function getUser(authUserId, data) {
+    return data.users.find(user => user.uId == authUserId)
+}
