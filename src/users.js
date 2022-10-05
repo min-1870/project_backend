8f842c51f08761cbafb12dcbec7a2eba1cca3b4f
Muhammad Lodhi <z5260271@ad.unsw.edu.au>
2022-10-06 09:37:37 +1100

    implementing userProfileV1 function

--- src/users.js
+++ src/users.js
@@ -1,7 +1,39 @@
-// userProfileV1 stub function
+import {
+    getData,
+  } from './dataStore.js'
 
+// userProfileV1 function
 export function userProfileV1( authUserId, uID ){
+    let data = getData();
+    let i = 0;  // checking if authUserId is valid
+    while (true) {
+        if (i >= data.users.length) {
+            return {error: 'authUserId is not valid'};
+        }
+        if (data.users[i]['uID'] === authUserId) {
+            break;
+        }  
+        i ++;
+    };
+
+    i = 0;  // checking if uID is valid
+    while (true) {
+        if (i >= data.users.length) {
+            return {error: 'uID is not valid'};
+        }
+        if (data.users[i]['uID'] === uID) {
+            break;
+        }  
+        i ++;
+    };
+
+
+    
     return {
-      channelId: 1,
+      uId: data.users[i]['uID'],
+      email: data.users[i]['email'],
+      nameFirst: data.users[i]['nameFirst'],
+      nameLast: data.users[i]['nameLast'],
+      handleStr: data.users[i]['handleStr']
     }
 }
--- src/users.test.js
+++ src/users.test.js
@@ -20,15 +20,15 @@ describe('Test Suite for userProfileV1', () => {
         expect(userProfileV1((temp_1 + 4345), temp_2)).toStrictEqual({error: expect.any(String)});    
     });
 
-    test('Successful authRegisterV1', () => { 
+    test('Successful userProfileV1', () => { 
         const temp_1 = authRegisterV1('moniker@hotmail.com', 'pvssword', 'real', 'moniker');
         const temp_2 = authRegisterV1('gomugomu@hotmail.com', 'devilfruit', 'monkey', 'luffy');  
         expect(userProfileV1(temp_1, temp_2)).toStrictEqual({
             uId: temp_2,
             email: 'gomugomu@hotmail.com',
-            nameFirst: monkey,
-            nameLast: luffy,
-            handleStr: monkeyluffy
+            nameFirst: 'monkey',
+            nameLast: 'luffy',
+            handleStr: 'monkeyluffy'
         });    
     });
 
