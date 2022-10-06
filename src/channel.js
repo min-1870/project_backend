909548a95c9a5b404bd30a712d09ddb1c740fb09
Minseok_Kim <200134kms@gmail.com>
2022-10-06 07:22:08 +0700

    added the function channelMessagesV1 and fixed the test a bit

--- src/channel.js
+++ src/channel.js
@@ -1,3 +1,4 @@
+import { getData } from './dataStore'
 // channelDetailsV1 stub function
 function channelDetailsV1(authUserId, channelId) {
   return {
@@ -37,17 +38,38 @@ function channelInviteV1(authUserId, channelId) {
 }
 
 //channelMessagesV1 stub function
-function channelMessagesV1 ( authUserId, channelId, start ) {
+export function channelMessagesV1 ( authUserId, channelId, start ) {
+  let data = getData()
+
+  if (data.channels.find(channel => channel.channelId == channelId) == null){   //if the channel Id is not exist, return error
+    return {error: "Invalid channel ID"}
+
+  }else if (data.users.find(user => user.uId == authUserId) == null){           //if the user Id is not exist, return error
+    return {error: "Invalid user ID"}
+
+  }else if(start < 0 || start > data.channels.find(channel => channel.channelId == channelId).messages.length){                //if starting point is out of range, return error
+    return {error: "Invalid start"}
+
+  }else if(data.channels.find(channel => channel.channelId == channelId).allMembers.find(user => user.uId == authUserId)){    //if the user is not the member of the channel, return error
+    return {error: "Not a member of the channel"}
+
+  }
+
+  let messages = data.channels.find(channel => channel.channelId == channelId).messages
+  let slicedMessages
+  let end;
+
+  if(start + 50 >= messages.length){
+    end = -1
+    slicedMessages = messages.slice(start)
+  }else{
+    end = start + 50
+    slicedMessages = messages.slice(start, end)
+  }
+
   return {
-    messages: [
-      {
-        messageId: 1,
-        uId: 1,
-        message: 'Hello world',
-        timeSent: 1582426789,
-      }
-    ],
-    start: 0,
-    end: 50,
+    messages: slicedMessages,
+    start: start,
+    end: end,
   }
 }
