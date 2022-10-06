0e7ed1d39c76e16d4232733e4205e98fa142934d
Xingjian Dong <z5221888@ad.unsw.edu.au>
2022-10-07 01:27:38 +1100

    add channelInviteV1

--- src/channel.js
+++ src/channel.js
@@ -40,7 +40,7 @@ export function channelJoinV1(authUserId, channelId) {
     return (element.channelId === channelId && element.isPublic === false);
   });
   let GlobalOwner = data.users.filter((element) => {
-    return (element.uId === authUserId && element.GlobalOwner === true);
+    return (element.uId === authUserId && element.isGlobalOwner === true);
   });
   if (uIdList.length === 0 ||
     cIdList.length === 0 ||
@@ -56,10 +56,37 @@ export function channelJoinV1(authUserId, channelId) {
   return {};
 }
 
-//channelInviteV1 stub function
-function channelInviteV1(authUserId, channelId) {
-  return {
+//channelInviteV1 function
+export function channelInviteV1(authUserId, channelId, uId) {
+  let data = getData();
+  let uIdList = data.users.filter((element) => {
+    return element.uId === uId;
+  });
+  let authIdList = data.users.filter((element) => {
+    return element.uId === authUserId;
+  });
+  let cIdList = data.channels.filter((element) => {
+    return element.channelId === channelId;
+  });
+  let MemberInChannel = data.channels.filter((element) => {
+    return (element.channelId === channelId && element.allMembers.includes(uId));
+  });
+  let authorisedUserNotMember = data.channels.filter((element) => {
+    return (element.channelId === channelId && !element.allMembers.includes(authUserId));
+  });
+  if (uIdList.length === 0 ||
+    authIdList.length === 0 ||
+    cIdList.length === 0 ||
+    MemberInChannel === 1 ||
+    authorisedUserNotMember === 1) {
+    return { error: 'error' };
   }
+  for (const element of data.channels) {
+    if (channelId === element.channelId) {
+      element.allMembers.push(uId);
+    }
+  }
+  return {};
 }
 
 /** <channelMessagesV1>
@@ -79,20 +106,20 @@ function channelInviteV1(authUserId, channelId) {
   * @param {number} start - the index of the starting point
   * @returns {{messages: array, start: number, end: number}} - an object contains the messages and information of pages
 */
-export function channelMessagesV1 ( authUserId, channelId, start ) {
+export function channelMessagesV1(authUserId, channelId, start) {
   let data = getData()
 
-  if (data.channels.find(channel => channel.channelId == channelId) == null){   //if the channel Id is not exist, return error
-    return {error: "Invalid channel ID"}
+  if (data.channels.find(channel => channel.channelId == channelId) == null) {   //if the channel Id is not exist, return error
+    return { error: "Invalid channel ID" }
 
-  }else if (data.users.find(user => user.uId == authUserId) == null){           //if the user Id is not exist, return error
-    return {error: "Invalid user ID"}
+  } else if (data.users.find(user => user.uId == authUserId) == null) {           //if the user Id is not exist, return error
+    return { error: "Invalid user ID" }
 
-  }else if(start < 0 || start > data.channels.find(channel => channel.channelId == channelId).messages.length){                //if starting point is out of range, return error
-    return {error: "Invalid start"}
+  } else if (start < 0 || start > data.channels.find(channel => channel.channelId == channelId).messages.length) {                //if starting point is out of range, return error
+    return { error: "Invalid start" }
 
-  }else if(data.channels.find(channel => channel.channelId == channelId).allMembers.find(user => user.uId == authUserId) == null){    //if the user is not the member of the channel, return error
-    return {error: "Not a member of the channel"}
+  } else if (data.channels.find(channel => channel.channelId == channelId).allMembers.find(user => user.uId == authUserId) == null) {    //if the user is not the member of the channel, return error
+    return { error: "Not a member of the channel" }
 
   }
 
@@ -100,10 +127,10 @@ export function channelMessagesV1 ( authUserId, channelId, start ) {
   let slicedMessages
   let end;
 
-  if(start + 50 >= messages.length){
+  if (start + 50 >= messages.length) {
     end = -1
     slicedMessages = messages.slice(start)
-  }else{
+  } else {
     end = start + 50
     slicedMessages = messages.slice(start, end)
   }
