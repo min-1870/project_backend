e02b60b5a45046dd7489397da2f908155a39c53b
Muhammad Lodhi <z5260271@ad.unsw.edu.au>
2022-10-22 06:27:47 +1100

    tests for user/profile/v2

---
+++ tests/integration/users.integration.test.ts
@@ -0,0 +1,86 @@
+import { TokenClass } from 'typescript';
+import { authResponse } from '../../src/types';
+import {
+  parseJsonResponse,
+  OK,
+  sendGetRequestToEndpoint,
+  sendDeleteRequestToEndpoint,
+  sendPostRequestToEndpoint,
+} from './integrationTestUtils';
+
+
+const EMAIL = 'Bob123@gmail.com';
+const PASSWORD = '11223344';
+const NAME_FIRST = 'Barty';
+const NAME_LAST = 'Potter';
+let token: string;
+let uId: number;
+beforeEach(() => {
+  sendDeleteRequestToEndpoint('/clear/v1', {});
+let res = sendPostRequestToEndpoint('/auth/register/v2', {
+    email: EMAIL,
+    password: PASSWORD,
+    nameFirst: NAME_FIRST,
+    nameLast: NAME_LAST
+  });
+
+  let jsonResponse = parseJsonResponse(res) as unknown as authResponse;
+  token = jsonResponse.token;
+
+  res = sendPostRequestToEndpoint('/auth/register/v2', {
+    email: 'gomugomu@hotmail.com',
+    password: PASSWORD,
+    nameFirst: 'monkey',
+    nameLast: 'luffy'
+  });
+
+  jsonResponse = parseJsonResponse(res) as unknown as authResponse;
+  uId = jsonResponse.authUserId;
+});
+
+describe('HTTP tests for /user/profile/v2', () => {
+  // happy path
+  test('Successful /user/profile/v2', () => {
+    const res = sendGetRequestToEndpoint('/user/profile/v2', {
+      token: token,
+      uId: uId
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      uId: uId,
+      email: 'gomugomu@hotmail.com',
+      nameFirst: 'monkey',
+      nameLast: 'luffy',
+      handleStr: 'monkeyluffy'
+    });
+  });
+
+  test('error passing invalid uId', () => {
+    const res = sendGetRequestToEndpoint('/user/profile/v2', {
+      token: token,
+      uId: (uId+12133)
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: expect.any(String)
+    });
+  });
+
+  test('error passing invalid authUserId', () => {
+    const res = sendGetRequestToEndpoint('/user/profile/v2', {
+      token: (token+434),
+      uId: uId
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: expect.any(String)
+    });
+  });
+
+
+
+
+});
