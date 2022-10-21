52b0e21cda49db07e82d316ab276a778c04afe46
Annie Huynh <z5391709@ad.unsw.edu.au>
2022-10-21 19:26:48 +1100

    Update lint to run in tests folder and fix lint issues

--- tests/channels.test.ts
+++ tests/channels.test.ts
@@ -1,4 +1,4 @@
-import { channelsCreateV1,channelsListV1, channelsListAllV1 } from '../src/channels';
+import { channelsCreateV1, channelsListV1, channelsListAllV1 } from '../src/channels';
 import { authRegisterV1 } from '../src/auth';
 import { clearV1 } from '../src/other';
 import { getData, setData } from '../src/dataStore';
@@ -6,203 +6,190 @@ import { getData, setData } from '../src/dataStore';
 const channelCreatorId = 1;
 const channelName = 'Cat Channel';
 const channelCreator = {
-    uId: channelCreatorId,
-    namesFirst: 'Adam',
-    namesLast: 'Johnston',
-    email: 'test@gmail.com',
-    handleStr: 'adamjohnston',
-    password: 'test123'
-}
-const channelId = 10000;  
-
-
+  uId: channelCreatorId,
+  namesFirst: 'Adam',
+  namesLast: 'Johnston',
+  email: 'test@gmail.com',
+  handleStr: 'adamjohnston',
+  password: 'test123'
+};
+const channelId = 10000;
 
 describe('Test set for the function channelsCreate', () => {
+  beforeEach(() => {
+    // Before every test reset and add a new test user
+    clearV1();
+    authRegisterV1(channelCreator.email, channelCreator.password, channelCreator.namesFirst, channelCreator.namesLast);
+  });
 
-    beforeEach(() => {
-        // Before every test reset and add a new test user
-        clearV1()
-        authRegisterV1(channelCreator.email, channelCreator.password, channelCreator.namesFirst, channelCreator.namesLast);
+  test('create new public channel success', () => {
+    const createChannelResult:any = channelsCreateV1(channelCreatorId, channelName, true);
+    expect(createChannelResult).toStrictEqual({
+      channelId: expect.any(Number)
     });
 
-    test('create new public channel success', () => {
-        const createChannelResult:any = channelsCreateV1(channelCreatorId, channelName, true)
-        expect(createChannelResult).toStrictEqual({
-            channelId: expect.any(Number)
-        });
-
-        expect(channelsListV1(channelCreatorId)).toStrictEqual({
-            channels: [
-                {
-                    channelId: createChannelResult.channelId,
-                    name: channelName,
-                }
-            ]
-        });
-
-        expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
-            channels: [
-                {
-                    channelId: createChannelResult.channelId,
-                    name: channelName
-                }
-            ]
-        });
+    expect(channelsListV1(channelCreatorId)).toStrictEqual({
+      channels: [
+        {
+          channelId: createChannelResult.channelId,
+          name: channelName,
+        }
+      ]
     });
 
-    test('create new private channel success', () => {
-        const createChannelResult:any = channelsCreateV1(channelCreatorId, channelName, false)
+    expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
+      channels: [
+        {
+          channelId: createChannelResult.channelId,
+          name: channelName
+        }
+      ]
+    });
+  });
 
-        expect(createChannelResult).toStrictEqual({
-            channelId: expect.any(Number)
-        });
+  test('create new private channel success', () => {
+    const createChannelResult:any = channelsCreateV1(channelCreatorId, channelName, false);
 
-        expect(channelsListV1(channelCreatorId)).toStrictEqual({
-            channels: [
-                {
-                    channelId: createChannelResult.channelId,
-                    name: channelName,
-                }
-            ]
-        });
-
-        expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
-            channels: [
-                {
-                    channelId: createChannelResult.channelId,
-                    name: channelName
-                }
-            ]
-        });
+    expect(createChannelResult).toStrictEqual({
+      channelId: expect.any(Number)
     });
 
-    test('create new channel name less than 1 return error', () => {
-        const createChannelResult = channelsCreateV1(channelCreatorId, '', false)
-
-        expect(createChannelResult).toStrictEqual({
-            error: "name is not between 1 and 20 characters"
-        });
+    expect(channelsListV1(channelCreatorId)).toStrictEqual({
+      channels: [
+        {
+          channelId: createChannelResult.channelId,
+          name: channelName,
+        }
+      ]
     });
 
-    test('create new channel name more than 20 return error', () => {
-        const createChannelResult = channelsCreateV1(channelCreatorId, 'helloisitmeyoulookingforhelloagain', false)
-
-        expect(createChannelResult).toStrictEqual({
-            error: "name is not between 1 and 20 characters"
-        });
+    expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
+      channels: [
+        {
+          channelId: createChannelResult.channelId,
+          name: channelName
+        }
+      ]
     });
+  });
 
-    test('create new channel creator is invalid return error', () => {
-        const createChannelResult = channelsCreateV1(24092001, channelName, false)
+  test('create new channel name less than 1 return error', () => {
+    const createChannelResult = channelsCreateV1(channelCreatorId, '', false);
 
-        expect(createChannelResult).toStrictEqual({
-            error: 'Invalid user ID'
-        });
+    expect(createChannelResult).toStrictEqual({
+      error: 'name is not between 1 and 20 characters'
     });
+  });
+
+  test('create new channel name more than 20 return error', () => {
+    const createChannelResult = channelsCreateV1(channelCreatorId, 'helloisitmeyoulookingforhelloagain', false);
+
+    expect(createChannelResult).toStrictEqual({
+      error: 'name is not between 1 and 20 characters'
+    });
+  });
+
+  test('create new channel creator is invalid return error', () => {
+    const createChannelResult = channelsCreateV1(24092001, channelName, false);
+
+    expect(createChannelResult).toStrictEqual({
+      error: 'Invalid user ID'
+    });
+  });
 });
-
-
 
 describe('Test set for the function channelsList', () => {
+  beforeEach(() => {
+    // Before every test reset and add a new test user
+    clearV1();
+    authRegisterV1(channelCreator.email, channelCreator.password, channelCreator.namesFirst, channelCreator.namesLast);
+  });
 
-    beforeEach(() => {
-        // Before every test reset and add a new test user
-        clearV1()
-        authRegisterV1(channelCreator.email, channelCreator.password, channelCreator.namesFirst, channelCreator.namesLast);
+  test('channels list valid authUserId with channels returns list of channels', () => {
+    const data:any = getData();
+    data.channels.push({
+      channelId,
+      name: channelName,
+      allMembers: [channelCreator]
     });
+    setData(data);
+    const channelListResult = channelsListV1(channelCreatorId);
 
-    test('channels list valid authUserId with channels returns list of channels', () => {
-        let data:any = getData();
-        data.channels.push({
-            channelId,
-            name: channelName,
-            allMembers: [channelCreator]
-        });
-        setData(data);
-        const channelListResult = channelsListV1(channelCreatorId)
+    expect(channelListResult).toStrictEqual({
+      channels: [
+        {
+          channelId,
+          name: channelName
+        }
+      ]
+    });
+  });
 
-        expect(channelListResult).toStrictEqual({
-            channels: [
-                {
-                    channelId,
-                    name: channelName
-                }
-            ]
-        });
-    }); 
+  test('channels list valid authUserId with no channels returns empty list', () => {
+    const channelListResult = channelsListV1(channelCreatorId);
 
-    test('channels list valid authUserId with no channels returns empty list', () => {
-        const channelListResult = channelsListV1(channelCreatorId)
+    expect(channelListResult).toStrictEqual({
+      channels: []
+    });
+  });
 
-        expect(channelListResult).toStrictEqual({
-            channels: []
-        });
-    }); 
+  test('channels list invalid authUserId', () => {
+    const channelListResult = channelsListV1(24092001);
 
-    test('channels list invalid authUserId', () => {
-        const channelListResult = channelsListV1(24092001)
-
-        expect(channelListResult).toStrictEqual({
-            error: 'authUserId is not valid'
-        });
-    }); 
+    expect(channelListResult).toStrictEqual({
+      error: 'authUserId is not valid'
+    });
+  });
 });
-
-
 
 describe('Test set for the function channelsListAllV1', () => {
-    
-beforeEach(() => {
+  beforeEach(() => {
     // Before every test reset and add a new test user
-    clearV1()
+    clearV1();
     authRegisterV1(channelCreator.email, channelCreator.password, channelCreator.namesFirst, channelCreator.namesLast);
-});
+  });
 
-    test('channelsListAllV1 invalid authUserId', () => {
-
-      expect(channelsListAllV1(24092001)).toStrictEqual({
-          error: 'authUserId is not valid'
-      });
-    }); 
-
-    test('channelsListAllV1 valid authUserId returns empty list', () => {
-
-      expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
-        channels: []
-      });
-    }); 
-
-    test('channelsListAllV1 valid authUserId returns list of all channels', () => {
-        let data:any = getData();
-        data.channels.push({
-            channelId: channelId,
-            isPublic: true,
-            name: channelName,
-            ownerMembers: [channelCreator],
-            allMembers: [channelCreator]
-        });
-        data.channels.push({
-            channelId: channelId+1,
-            isPublic: false,
-            name: channelName+'2',
-            ownerMembers: [channelCreator],
-            allMembers: [channelCreator]
-        });
-        setData(data);
-
-        expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
-            channels: [
-                {
-                    channelId: channelId,
-                    name: channelName
-                },
-                {
-                    channelId: channelId+1,
-                    name: channelName+'2'
-                }
-            ]
-        });
-
+  test('channelsListAllV1 invalid authUserId', () => {
+    expect(channelsListAllV1(24092001)).toStrictEqual({
+      error: 'authUserId is not valid'
     });
-});
+  });
 
+  test('channelsListAllV1 valid authUserId returns empty list', () => {
+    expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
+      channels: []
+    });
+  });
+
+  test('channelsListAllV1 valid authUserId returns list of all channels', () => {
+    const data:any = getData();
+    data.channels.push({
+      channelId: channelId,
+      isPublic: true,
+      name: channelName,
+      ownerMembers: [channelCreator],
+      allMembers: [channelCreator]
+    });
+    data.channels.push({
+      channelId: channelId + 1,
+      isPublic: false,
+      name: channelName + '2',
+      ownerMembers: [channelCreator],
+      allMembers: [channelCreator]
+    });
+    setData(data);
+
+    expect(channelsListAllV1(channelCreatorId)).toStrictEqual({
+      channels: [
+        {
+          channelId: channelId,
+          name: channelName
+        },
+        {
+          channelId: channelId + 1,
+          name: channelName + '2'
+        }
+      ]
+    });
+  });
+});
