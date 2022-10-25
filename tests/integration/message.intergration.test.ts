b6682555b66b603610c25b44546d3ed853f7a1c8
Minseok_Kim <200134kms@gmail.com>
2022-10-25 23:47:53 +0700

    added test for messageRemove

--- tests/integration/message.intergration.test.ts
+++ tests/integration/message.intergration.test.ts
@@ -14,6 +14,9 @@ const NAME_LAST = 'Potter';
 const TEST_CHANNEL_NAME = 'Test channel';
 const TEST_MESSAGE = 'hello world :)';
 
+const TEST_INVALID_TOKEN = '999999999999';
+const TEST_INVALID_CHANNELID = '999999999999';
+
 // over 1000 characters
 let VERY_LONG_MESSAGE = ':(';
 for (let i = 0; i < 1000; i++) VERY_LONG_MESSAGE = VERY_LONG_MESSAGE + ':(';
@@ -44,7 +47,7 @@ beforeEach(() => {
   // authUserId2 = (parseJsonResponse(res2) as unknown as authResponse).authUserId;
 });
 
-describe('HTTP tests for channel/messages/v2', () => {
+describe('HTTP tests for message/send/v1', () => {
   let channel1Id: number;
   beforeEach(() => {
     const channel1Res = sendPostRequestToEndpoint('/channels/create/v2', {
@@ -58,7 +61,7 @@ describe('HTTP tests for channel/messages/v2', () => {
   test('channelId does not refer to a valid channel', () => {
     const res = sendPostRequestToEndpoint('/message/send/v1', {
       token: token,
-      channelId: 9999999,
+      channelId: TEST_INVALID_CHANNELID,
       message: TEST_MESSAGE,
     });
 
@@ -109,7 +112,7 @@ describe('HTTP tests for channel/messages/v2', () => {
 
   test('token is invalid', () => {
     const res = sendPostRequestToEndpoint('/message/send/v1', {
-      token: '999999',
+      token: TEST_INVALID_TOKEN,
       channelId: channel1Id,
       message: TEST_MESSAGE,
     });
@@ -171,5 +174,96 @@ describe('HTTP tests for channel/messages/v2', () => {
 
     expect(res.statusCode).toBe(OK);
     expect((parseJsonResponse(res2) as unknown as channelMessagesOutput).messages[0].timeSent).toBeLessThanOrEqual(Date.now() + 2);
+  });
+});
+
+describe('HTTP tests for message/remove/v1', () => {
+  let testChannelId: number;
+  let testMessageId: number;
+  beforeEach(() => {
+    const channel = sendPostRequestToEndpoint('/channels/create/v2', {
+      token: token,
+      name: TEST_CHANNEL_NAME,
+      isPublic: true
+    });
+    testChannelId = (parseJsonResponse(channel) as unknown as channelId).channelId;
+
+    const res2 = sendPostRequestToEndpoint('/message/send/v1', {
+      token: token,
+      channelId: testChannelId,
+      message: TEST_MESSAGE,
+    });
+    testMessageId = (parseJsonResponse(res2) as unknown as messageId).messageId;
+  });
+
+  test('token is invalid', () => {
+    const res = sendDeleteRequestToEndpoint('/message/remove/v1', {
+      token: TEST_INVALID_TOKEN,
+      messageId: testMessageId
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'token is invalid'
+    });
+  });
+
+  test('messageId does not refer to a valid message within a channel/DM that the authorised user has joined', () => {
+    const res = sendDeleteRequestToEndpoint('/message/remove/v1', {
+      token: token2,
+      messageId: testMessageId
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'messageId does not refer to a valid message within a channel/DM that the authorised user has joined'
+    });
+  });
+
+  test('the message was not sent by the authorised user making this request and the user does not have owner permissions in the channel/DM', () => {
+    sendPostRequestToEndpoint('/channel/join/v2', {
+      token: token2,
+      channelId: testChannelId
+    });
+    
+    const res = sendDeleteRequestToEndpoint('/message/remove/v1', {
+      token: token2,
+      messageId: testMessageId
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'the message was not sent by the authorised user making this request and the user does not have owner permissions in the channel/DM'
+    });
+  });
+
+  test('correct input correct return', () => {
+    const res = sendDeleteRequestToEndpoint('/message/remove/v1', {
+      token: token,
+      messageId: testMessageId
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({});
+  });
+
+  test('correct input correct channel/message', () => {
+    sendDeleteRequestToEndpoint('/message/remove/v1', {
+      token: token,
+      messageId: testMessageId
+    });
+
+    const res = sendGetRequestToEndpoint('/channel/messages/v2', {
+      token: token,
+      channelId: testChannelId,
+      start: 0,
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      messages: [],
+      start: 0,
+      end: -1
+    });
   });
 });
