b61542c30598bd91163b2e3f0566b27fdfb0b600
Minseok_Kim <200134kms@gmail.com>
2022-10-26 22:33:31 +0700

    implemented the function message edit v1

--- src/message.ts
+++ src/message.ts
@@ -49,3 +49,25 @@ export function messageRemove (authUserId: number, messageId: number): (Record<s
 
   return {};
 }
+
+export function messageEdit (authUserId: number, messageId: number, message: string): (Record<string, never> | error) {
+  // assume toke is valid
+  const data:dataStore = getData();
+  const channelId = findChannelIdByMessageId(messageId, data);
+
+  if (message.length < 1 || message.length > 1000) {
+    return { error: 'length of message is less than 1 or over 1000 characters' };
+  } else if (!isMessageIdValid(messageId, data)) {
+    return { error: 'messageId does not refer to a valid message within a channel/DM that the authorised user has joined' };
+  } else if (!isUserMemberInChannel(authUserId, channelId, data)) {
+    return { error: 'messageId does not refer to a valid message within a channel/DM that the authorised user has joined' };
+  } else if (authUserId !== getDataStoreMessage(messageId, data).uId && !isUserOwnerMemberInChannel(authUserId, channelId, data)) {
+    return { error: 'the message was not sent by the authorised user making this request and the user does not have owner permissions in the channel/DM' };
+  }
+
+  const dataStoreChannel = getDataStoreChannel(channelId, data);
+  const editedMessage: messages = dataStoreChannel.messages.find(message => message.messageId === messageId);
+  editedMessage.message = message;
+
+  return {};
+}
