ebcc54925b3d35948578f3060f3fa427c065538c
Minseok_Kim <200134kms@gmail.com>
2022-10-26 22:24:15 +0700

    wrote test set for the function message edit

--- tests/integration/message.intergration.test.ts
+++ tests/integration/message.intergration.test.ts
@@ -4,7 +4,8 @@ import {
   parseJsonResponse,
   sendDeleteRequestToEndpoint,
   sendGetRequestToEndpoint,
-  sendPostRequestToEndpoint
+  sendPostRequestToEndpoint,
+  sendPutRequestToEndpoint
 } from './integrationTestUtils';
 
 const EMAIL = 'Bob123@gmail.com';
@@ -13,6 +14,7 @@ const NAME_FIRST = 'Barty';
 const NAME_LAST = 'Potter';
 const TEST_CHANNEL_NAME = 'Test channel';
 const TEST_MESSAGE = 'hello world :)';
+const TEST_MESSAGE_2 = 'hello world :(';
 
 const TEST_INVALID_TOKEN = '999999';
 const TEST_INVALID_CHANNELID = '99999';
@@ -262,6 +264,128 @@ describe('HTTP tests for message/remove/v1', () => {
     expect(res.statusCode).toBe(OK);
     expect(parseJsonResponse(res)).toStrictEqual({
       messages: [],
+      start: 0,
+      end: -1
+    });
+  });
+});
+
+describe('HTTP tests for message/edit/v1', () => {
+  let testChannelId: number;
+  let testMessageId: number;
+  beforeEach(() => {
+    const channel = sendPostRequestToEndpoint('/channels/create/v2', {
+      token: token,
+      name: TEST_CHANNEL_NAME,
+      isPublic: true
+    });
+    testChannelId = (parseJsonResponse(channel) as unknown as channelId).channelId;
+
+    const res2 = sendPostRequestToEndpoint('/message/send/v1', {
+      token: token,
+      channelId: testChannelId,
+      message: TEST_MESSAGE,
+    });
+    testMessageId = (parseJsonResponse(res2) as unknown as messageId).messageId;
+  });
+
+  test('length of message is over 1000 characters', () => {
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: token,
+      messageId: testMessageId,
+      message: VERY_LONG_MESSAGE
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'length of message is less than 1 or over 1000 characters'
+    });
+  });
+
+  test('length of message is less than 1 characters', () => {
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: token,
+      messageId: testMessageId,
+      message: ''
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'length of message is less than 1 or over 1000 characters'
+    });
+  });
+
+  test('messageId does not refer to a valid message within a channel/DM that the authorised user has joined', () => {
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: token2,
+      messageId: testMessageId,
+      message: TEST_MESSAGE_2
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'messageId does not refer to a valid message within a channel/DM that the authorised user has joined'
+    });
+  });
+
+  test('the message was not sent by the authorised user making this request and the user does not have owner permissions in the channel/DM', () => {
+    sendPostRequestToEndpoint('/channel/join/v2', {
+      token: token2,
+      channelId: testChannelId
+    });
+
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: token2,
+      messageId: testMessageId,
+      message: TEST_MESSAGE_2
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'the message was not sent by the authorised user making this request and the user does not have owner permissions in the channel/DM'
+    });
+  });
+
+  test('token is invalid', () => {
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: TEST_INVALID_TOKEN,
+      messageId: testMessageId,
+      message: TEST_MESSAGE_2
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({
+      error: 'token is invalid'
+    });
+  });
+
+  test('correct input, correct return', () => {
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: token,
+      messageId: testMessageId,
+      message: TEST_MESSAGE_2
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({});
+  });
+
+  test('correct input, correct message', () => {
+    const res = sendPutRequestToEndpoint('/message/edit/v1', {
+      token: token,
+      messageId: testMessageId,
+      message: TEST_MESSAGE_2
+    });
+
+    const res2 = sendGetRequestToEndpoint('/channel/messages/v2', {
+      token: token,
+      channelId: testChannelId,
+      start: 0,
+    });
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res2)).toStrictEqual({
+      messages: [{ messageId: testMessageId, uId: authUserId, message: TEST_MESSAGE_2, timeSent: expect.any(Number) }],
       start: 0,
       end: -1
     });
