3c47069bf9ec920aebc580c8fc6f184fee7de8fe
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-11-02 15:58:29 +1100

    Update to deployment script using --omit=dev

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ USERNAME="fri09aboost"
 SSH_HOST="ssh-fri09aboost.alwaysdata.net"
 
 scp -r ./package.json ./package-lock.json ./tsconfig.json ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
-ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && npm install --only=production"
+ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && npm install --omit=dev"
