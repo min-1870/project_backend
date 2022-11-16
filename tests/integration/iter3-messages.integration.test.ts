076d42bb99edee580f404d12bc100ba325465cce
Muhammad Lodhi <z5260271@ad.unsw.edu.au>
2022-11-16 15:49:12 +1100

    adding tests for messages/unpin/v1

--- tests/integration/iter3-messages.integration.test.ts
+++ tests/integration/iter3-messages.integration.test.ts
@@ -94,8 +94,108 @@ describe('HTTP tests for message/pin/v1', () => {
     expect(bodyObj.error).toStrictEqual({ message: 'user does not have owner permissions in the channel/DM.' });
   });
 
+  test('Message is already pinned', () => {
+    sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+    const res = sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(400);
+    expect(bodyObj.error).toStrictEqual({ message: 'Message is already pinned.' });
+  });
+
   test('correct input correct return', () => {
     const res = sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+
+    expect(res.statusCode).toBe(OK);
+    expect(parseJsonResponse(res)).toStrictEqual({});
+  });
+});
+
+describe('HTTP tests for message/unpin/v1', () => {
+  let testChannelId: number;
+  let testMessageId: number;
+  beforeEach(() => {
+    const channel = sendPostRequestToEndpoint('/channels/create/v3', {
+      name: TEST_CHANNEL_NAME,
+      isPublic: true
+    }, token);
+    testChannelId = (parseJsonResponse(channel) as unknown as channelId).channelId;
+
+    const res2 = sendPostRequestToEndpoint('/message/send/v2', {
+      channelId: testChannelId,
+      message: TEST_MESSAGE,
+    }, token);
+    testMessageId = (parseJsonResponse(res2) as unknown as messageId).messageId;
+  });
+
+  test('Message is not pinned', () => {
+    const res = sendPostRequestToEndpoint('/message/unpin/v1', {
+      messageId: testMessageId
+    }, token);
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(400);
+    expect(bodyObj.error).toStrictEqual({ message: 'Message is not pinned.' });
+  });
+
+  test('token is invalid', () => {
+    sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+
+    const res = sendPostRequestToEndpoint('/message/unpin/v1', {
+      messageId: testMessageId
+    }, TEST_INVALID_TOKEN);
+
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(403);
+    expect(bodyObj.error).toStrictEqual({ message: 'Invalid token.' });
+  });
+
+  test('messageId does not refer to a valid message within a channel/DM that the authorised user has joined', () => {
+    sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+
+    const res = sendPostRequestToEndpoint('/message/unpin/v1', {
+      messageId: testMessageId
+    }, token2);
+
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(400);
+    expect(bodyObj.error).toStrictEqual({ message: 'User is not part of the channel where this message is.' });
+  });
+
+  test('user does not have owner permissions in the channel/DM', () => {
+    const joinRes = sendPostRequestToEndpoint('/channel/join/v3', {
+      channelId: testChannelId
+    }, token2);
+
+    expect(joinRes.statusCode).toBe(OK);
+
+    sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+
+    const res = sendPostRequestToEndpoint('/message/unpin/v1', {
+      messageId: testMessageId
+    }, token2);
+
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(403);
+    expect(bodyObj.error).toStrictEqual({ message: 'user does not have owner permissions in the channel/DM.' });
+  });
+
+  test('correct input correct return', () => {
+    sendPostRequestToEndpoint('/message/pin/v1', {
+      messageId: testMessageId
+    }, token);
+
+    const res = sendPostRequestToEndpoint('/message/unpin/v1', {
       messageId: testMessageId
     }, token);
 
